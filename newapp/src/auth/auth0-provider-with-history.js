ce832a527214a4979d2c75ae70cf1248104a787d
blackPrincess31 <katesavchenko31@gmail.com>
2021-12-28 20:47:36 +0300

    problem found

--- newapp/src/auth/auth0-provider-with-history.js
+++ newapp/src/auth/auth0-provider-with-history.js
@@ -1,30 +1,30 @@
-// import React from 'react';
-// import { useHistory } from 'react-router-dom';
-// import { Auth0Provider } from '@auth0/auth0-react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-// const Auth0ProviderWithHistory = ({ children }) => {
-//   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-//   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const Auth0ProviderWithHistory = ({ children }) => {
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-//   const history = useHistory();
+  const history = useHistory();
 
-//   const onRedirectCallback = (appState) => {
-//     history.push(appState?.returnTo || window.location.pathname);
-//   };
+  const onRedirectCallback = (appState) => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
 
-//   return (
-//     <Auth0Provider
-//       domain={domain}
-//       clientId={clientId}
-//       redirectUri={window.location.origin}
-//       onRedirectCallback={onRedirectCallback}
-//     >
-//       {children}
-//     </Auth0Provider>
-//   );
-// };
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
 
-// export default Auth0ProviderWithHistory;
+export default Auth0ProviderWithHistory;
 
 
 //метод onRedirectCallback () для обработки события, когда Auth0 перенаправляет пользователей со страницы Auth0 в ваше приложение React
--- newapp/src/auth/protected-route.js
+++ newapp/src/auth/protected-route.js
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import { Route } from 'react-router-dom';
-// import { withAuthenticationRequired } from '@auth0/auth0-react';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
 
 
-// const ProtectedRoute = ({ component, ...args }) => (
-//   <Route
-//     component={withAuthenticationRequired(component,
-//       {
-//       onRedirecting: () => <Loading />,
-//      })}
-//     {...args}
-//   />
-// );
+const ProtectedRoute = ({ component, ...args }) => (
+  <Route
+    component={withAuthenticationRequired(component,
+      {
+      onRedirecting: () => <Loading />,
+     })}
+    {...args}
+  />
+);
 
 
 
-// export default ProtectedRoute;
+export default ProtectedRoute;
